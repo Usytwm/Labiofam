fe3f5e7161ed07b17d4338b16fe0d7d80f18edf9
Brian <gisameht@gmail.com>
2023-10-10 22:26:20 -0400

    enviroments

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Cors
+builder.Services.AddCors(options => options.AddPolicy("AllowWebApp", builder => builder
+.AllowAnyOrigin()
+.AllowAnyHeader()
+.AllowAnyMethod()));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +21,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//Usecors
+app.UseCors("AllowWebApp");
 
 app.UseHttpsRedirection();
 
