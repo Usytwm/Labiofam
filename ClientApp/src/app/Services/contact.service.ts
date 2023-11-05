bf5111fb339ea857b68359b0805f405e9d186636
Brian <gisameht@gmail.com>
2023-11-04 22:35:37 -0400

    add clientservice

--- ClientApp/src/app/Services/contact.service.ts
+++ ClientApp/src/app/Services/contact.service.ts
@@ -1,14 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AbstractService } from './generic-crud.service';
-import { Client } from '../Interfaces/Client';
-import { HttpClient } from '@angular/common/http';
+import { Contact } from '../Interfaces/Contact';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ClientService extends AbstractService<Client> {
+export class ContactService extends AbstractService<Contact> {
   constructor(_http: HttpClient) {
     super(_http);
-    this.apiUrl = 'api/Client/';
+    this.apiUrl = 'api/Contact/';
   }
 }
