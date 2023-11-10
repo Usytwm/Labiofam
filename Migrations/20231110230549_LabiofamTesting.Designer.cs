08f2ea5ec7ad3168809e2bb3f76ce5f8e0645678
Brian <gisameht@gmail.com>
2023-11-10 18:06:44 -0500

    Update database password in appsettings.json

--- Migrations/20231110230549_LabiofamTesting.Designer.cs
+++ Migrations/20231110230549_LabiofamTesting.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Labiofam.Migrations
 {
     [DbContext(typeof(WebDbContext))]
-    [Migration("20231110220636_LabiofamTesting")]
+    [Migration("20231110230549_LabiofamTesting")]
     partial class LabiofamTesting
     {
         /// <inheritdoc />
