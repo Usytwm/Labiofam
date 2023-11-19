758f17db774cb535735dce1ce531abcdff6e6837
Brian <gisameht@gmail.com>
2023-11-18 21:14:11 -0500

    quitar info de los cruds y algunos detalles

--- ClientApp/src/app/admin/users-admin/components/users-admin/users-admin.component.ts
+++ ClientApp/src/app/admin/users-admin/components/users-admin/users-admin.component.ts
@@ -2,7 +2,9 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
+import { forkJoin, map } from 'rxjs';
 import { RegistrationModel } from 'src/app/Interfaces/registration-model';
+import { UserRoleFilterService } from 'src/app/Services/FilterServices/user-roles-filter.service';
 import { RegistrationService } from 'src/app/Services/RegistrationsService/registration.service';
 @Component({
   selector: 'app-users-admin',
@@ -19,7 +21,8 @@ export class UsersAdminComponent implements OnInit {
 
   constructor(
     private _snackBar: MatSnackBar,
-    private _registrationervice: RegistrationService
+    private _registrationervice: RegistrationService,
+    private _filter:UserRoleFilterService
   ) {}
 
   ngOnInit() {
@@ -28,18 +31,29 @@ export class UsersAdminComponent implements OnInit {
 
   getAll(): void {
     this.loading = true;
-    this._registrationervice.getAll().subscribe((data) => {
-      this._dataColumns = {
-        id: 'id',
-        nombre: 'userName',
-        roles: 'roles',
-      };
-      this._data = data.map((item) => ({
-        ...item,
-        roles: 'knjsn',
-      }));
-      this.loading = false;
+    this._registrationervice.getAll().subscribe((users) => {
+      // Crear un array de observables para cada usuario
+      const observables = users.map((user) =>
+        this._filter.getType1byType2(user.id!).pipe(
+          map((roles) => ({
+            ...user,
+            roles: roles.map(x=>x.name).join(', '), // Aquí se agregan los roles al usuario
+          }))
+        )
+      );
+    
+      // Utilizar forkJoin para esperar a que todos los observables se completen
+      forkJoin(observables).subscribe((data) => {
+        this._dataColumns = {
+          id: 'id',
+          nombre: 'userName',
+          roles: 'roles',
+        };
+        this._data = data; // Ahora data contiene los usuarios con sus roles
+        this.loading = false;
+      });
     });
+    
   }
 
   Delete(id: string) {
