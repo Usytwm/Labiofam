78631cdf0ed6ff36e37efcb46d3d79e301f743ea
LeoRamirez1405 <leojavramirez1405@gmail.com>
2023-11-23 11:41:40 -0500

    Vista Testimonios

--- ClientApp/src/app/public/public.module.ts
+++ ClientApp/src/app/public/public.module.ts
@@ -7,6 +7,7 @@ import { PointsOfSalesModule } from './establecimientos/points-of-sales.module';
 import { ServicesModule } from './servicios/servicios.module';
 import { ContactosModule } from './contactos/contactos.module';
 import { BioproductosModule } from './bioproductos/bioproductos.module';
+import { TestimoniosModule } from './testimonios/testimonios.module';
 
 //componentes
 import { NavComponent } from './components/nav/nav.component';
@@ -24,6 +25,7 @@ import { PublicComponent } from './public.component';
     ContactosModule,
     BioproductosModule,
     FontAwesomeModule,
+    TestimoniosModule,
   ],
 })
 export class PublicModule {}
---
+++ ClientApp/src/app/public/testimonios/components/home.testimonios/testimonios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonios',
+  templateUrl: './testimonios.component.html',
+  styleUrls: ['./testimonios.component.css']
+})
+export class TestimoniosComponent {
+
+}
