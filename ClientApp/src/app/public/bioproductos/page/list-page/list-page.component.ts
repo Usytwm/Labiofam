caddad23d354081ee1a907eb130ca123645e16fe
Eric <eric.tornas@gmail.com>
2023-11-27 23:21:30 -0600

    filtro por nombre

--- ClientApp/src/app/public/bioproductos/page/list-page/list-page.component.ts
+++ ClientApp/src/app/public/bioproductos/page/list-page/list-page.component.ts
@@ -1,4 +1,4 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Product } from 'src/app/Interfaces/Product';
 import { ProductService } from 'src/app/Services/EntitiesServices/product.service';
 import { RouterLink } from '@angular/router';
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
@@ -18,24 +19,26 @@ export class ListPageComponent {
   pageSize: number = 9;
   page: number = 1;
 
+  filteredProducts: Product[] = [];
+
   dataSource!: MatTableDataSource<Product>;
   dataLoaded: boolean = false;
   existobjects: boolean = true;
 
   @ViewChild(MatPaginator) paginator?: MatPaginator;
   @ViewChild(MatSort) sort?: MatSort;
+
   constructor(private _bioproductsservices: ProductService) {}
+
   ngOnInit(): void {
     this.obtenerBioproductos();
-    this.dataSource = new MatTableDataSource<Product>(this.bioproductos);
-    this.dataSource.paginator = this.paginator!;
-    this.dataSource.sort = this.sort!;
   }
+
   obtenerBioproductos() {
     this.loading = true;
     this._bioproductsservices.getAll().subscribe((data) => {
       this.bioproductos = data;
-      console.log(data);
+      this.filteredProducts = this.bioproductos;
       this.loading = false;
     });
   }
@@ -43,9 +46,12 @@ export class ListPageComponent {
   handlePageChange(event: number) {
     this.page = event;
   }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.filteredProducts = this.bioproductos.filter((product) => {
+      return product.name?.toLowerCase().includes(filterValue.toLowerCase());
+    });
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
