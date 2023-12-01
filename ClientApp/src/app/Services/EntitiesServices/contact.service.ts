1493bb81f1aa282bf4999f53d9a8da8add148ccc
Eric <eric.tornas@gmail.com>
2023-12-01 01:37:11 -0600

    Provisional

--- ClientApp/src/app/Services/EntitiesServices/contact.service.ts
+++ ClientApp/src/app/Services/EntitiesServices/contact.service.ts
@@ -2,9 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Contact } from '../../Interfaces/Contact';
 import { EntityAbstractService } from '../BaseServices/entity-abstract.service';
-import { Observable, of, catchError } from 'rxjs';
+import { Observable, of, catchError, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
-import Swal from 'sweetalert2';
+import Swal from 'sweetalert2'; // Import Swal module
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +15,27 @@ export class ContactService extends EntityAbstractService<Contact> {
     this.apiUrl = 'api/Contact/';
   }
 
+  upPhoto(archivo: File, id: string): Observable<Contact> {
+    let formData = new FormData();
+    formData.append("archivo", archivo);
+    formData.append("id", id);
+
+    return this._http.post(`${this.appUrl}/upload/`, formData).pipe(
+      map((response: any) => response.contacto as Contact),
+      catchError((e) => {
+        console.error(e.error.mensaje);
+
+        // Access Swal function from the imported module
+        Swal.fire(e.error.mensaje, e.error.error, 'error');
+
+        return throwError(e);
+      })
+    );
+  }
+
+
+
+  /*
   uploadPhoto(fotoBase64: string, id: string, filename: string): Observable<Contact | null> {
     const formData = new FormData();
     formData.append('foto', fotoBase64);
@@ -29,5 +50,5 @@ export class ContactService extends EntityAbstractService<Contact> {
       })
     );
   }
-
+  */
 }
--- ClientApp/src/app/admin/contacts-admin/components/add-edit-contacts/add-edit-contacts.component.ts
+++ ClientApp/src/app/admin/contacts-admin/components/add-edit-contacts/add-edit-contacts.component.ts
@@ -6,6 +6,8 @@ import { Contact } from 'src/app/Interfaces/Contact';
 import { ContactService } from 'src/app/Services/EntitiesServices/contact.service';
 import { Observable } from 'rxjs';
 import Swal from 'sweetalert2';
+import { DomSanitizer } from '@angular/platform-browser';
+//import { blob } from 'stream/consumers';
 
 @Component({
   selector: 'app-add-edit-contacts',
@@ -32,7 +34,8 @@ export class AddEditContactsComponent {
     private contactService: ContactService,
     private snackBar: MatSnackBar,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer
   ) {
     this.id = String(this.route.snapshot.paramMap.get('id'));
   }
@@ -43,7 +46,17 @@ export class AddEditContactsComponent {
       this.getContact(this.id);
     }
   }
-
+  onFileSelected(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.files && target.files.length) {
+      const file = target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imagePreview = reader.result as string;
+      };
+    }
+  }
   getContact(id: string) {
     this.loading = true;
     this.contactService.get(id).subscribe((data) => {
@@ -84,7 +97,81 @@ export class AddEditContactsComponent {
       this.router.navigate(['/dashboard/contacts-admin']);
     });
   }
-  /*
+  newContact(): Contact {
+    const imageName = this.imagePreview?.split('/').pop();
+    const imagePath = imageName ? `assets/${imageName}` : '';
+    return {
+      name: this.form.value.name!,
+      occupation: this.form.value.occupation!,
+      contact_Info: this.form.value.info!,
+      image: imagePath,
+    };
+  }
+
+
+
+
+
+
+}
+/*
+  upPhoto() {
+    if (this.contacto && this.imagePreview) {
+      const randomFilename = `${Math.random().toString(36).substr(2, 9)}.jpg`;
+      this.contactService
+        .UploadPhoto(this.imagePreview, this.contacto.id, randomFilename)
+        .subscribe((contacto: Contact) => {
+          this.contacto = contacto;
+          Swal.fire('La foto se ha subido con éxito');
+        });
+    } else if (!this.contacto) {
+      Swal.fire('Error', 'No se ha encontrado el contacto', 'error');
+    } else if (!this.imagePreview) {
+      Swal.fire('Error', 'No se ha seleccionado una foto', 'error');
+    }
+  }
+  capturarFile(event): any {
+    const archivos = event.target.files[0]
+    this.extraerBase64(archivos).then(imagen => {
+      this
+      console.log(imagen);
+    })
+  }
+  extraerBase64 = async ($event: any) => new Promise((resolve, reject) => {
+    try {
+      const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          blob: $event,
+          image,
+          base:reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          blob:$event,
+          image,
+          base:null
+        });
+      };
+
+    } catch (e) {
+        return null;
+    }
+  })
+ }
+
+
+
+
+
+  */
+
+
+ /*
   newContact(): Contact {
     return {
       name: this.form.value.name!,
@@ -94,7 +181,22 @@ export class AddEditContactsComponent {
     };
   }
   */
+ /*
+  onFileSelected(event: Event){
+    this.foto = event.target.files[0]
+    console.log(this.foto);
+  }
+  subirFoto(){
+    this.contactService.upPhoto(this.foto, this.contacto.id).subscribe(this.contacto => {
+      this.contacto = contacto;
+      Swal('foto!',)
+    });
+  }
+  */
 
+
+
+  /*
   onFileSelected(event: Event) {
     const target = event.target as HTMLInputElement;
     if (target.files && target.files.length) {
@@ -106,7 +208,7 @@ export class AddEditContactsComponent {
       };
     }
   }
-
+  */
   /*
   onFileSelected(event: Event) {
     const target = event.target as HTMLInputElement;
@@ -134,34 +236,3 @@ export class AddEditContactsComponent {
     }
   }
   */
-
-
-
-  newContact(): Contact {
-    const imageName = this.imagePreview?.split('/').pop();
-    const imagePath = imageName ? `assets/${imageName}` : '';
-    return {
-      name: this.form.value.name!,
-      occupation: this.form.value.occupation!,
-      contact_Info: this.form.value.info!,
-      image: imagePath,
-    };
-  }
-
-}
-/*
-  upPhoto() {
-    if (this.contacto && this.imagePreview) {
-      const randomFilename = `${Math.random().toString(36).substr(2, 9)}.jpg`;
-      this.contactService
-        .UploadPhoto(this.imagePreview, this.contacto.id, randomFilename)
-        .subscribe((contacto: Contact) => {
-          this.contacto = contacto;
-          Swal.fire('La foto se ha subido con éxito');
-        });
-    } else if (!this.contacto) {
-      Swal.fire('Error', 'No se ha encontrado el contacto', 'error');
-    } else if (!this.imagePreview) {
-      Swal.fire('Error', 'No se ha seleccionado una foto', 'error');
-    }
-  } */
