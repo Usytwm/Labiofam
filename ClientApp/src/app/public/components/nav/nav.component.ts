7ec63c8fc62d6eda844c9124d72c2f6f7ff4abbd
LeoRamirez1405 <leojavramirez1405@gmail.com>
2023-11-30 23:53:20 -0500

    Cositas del home servicios

--- ClientApp/src/app/public/components/nav/nav.component.ts
+++ ClientApp/src/app/public/components/nav/nav.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { ServicesComponent } from './../../servicios/components/servicios/servicios.component'
+import {ServicesService} from './../../../Services/EntitiesServices/services.service'
+import {Service} from './../../../Interfaces/Service'
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
+ servicios: Service[] = [];
 
+  constructor(private _servicesservices: ServicesService) {}
+  ngOnInit(): void {
+    this.obtenerServicios();
+  }
+
+  obtenerServicios() {
+    this._servicesservices.getAll().subscribe((data) => {
+      this.servicios = data;
+      console.log(data);
+    });
+  }
 }
